3afdacb155611b30debff483f208e3b5bdeb4b34
chika-12 <markworship001@gmail.com>
2023-10-07 15:27:46 -0700

    simple shell

---
+++ betty_code.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ * main - Processing betty code
+ *
+ * Return: Always 0:
+ */
+int main(void)
+{
+	int index;
+
+	for (index = 0; index <= 10; index++)
+	{
+		printf("%d ", index);
+	}
+	putchar(10);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#endif
