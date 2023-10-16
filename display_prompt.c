b4c297019d2571033b75a733c0b597365557cf15
chika-12 <markworship001@gmail.com>
2023-10-16 20:59:45 +0100

    simple shell

--- display_prompt.c
+++ display_prompt.c
@@ -8,7 +8,6 @@
 void display_prompt(void)
 {
 	char ihe_ntiye[100];
-	char *box[] = {"ls", "pwd", "ls -l", "rm", "rmdir", "cd", "rm -r"};
 	int index;
 
 	while (1)
@@ -28,19 +27,7 @@ void display_prompt(void)
 
 		if (ihe_ntiye[0] != '\0')
 		{
-			for (index = 0; box[index] != NULL; index++)
-			{
-				if (strcmp(box[index], ihe_ntiye) == 0)
-				{
-					execute_ozi(ihe_ntiye);
-					break;
-				}
-			}
-
-			if (box[index] == NULL)
-			{
-				printout("%s\n", ihe_ntiye);
-			}
+			execute_ozi(ihe_ntiye);
 
 		}
 	}
