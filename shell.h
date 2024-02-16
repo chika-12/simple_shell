ae3e7e1793fe3050cdcfb82da04c21d1ccc51dd9
chika-12 <markworship001@gmail.com>
2024-02-16 09:38:34 +0100

    shell

--- shell.h
+++ shell.h
@@ -9,16 +9,5 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdarg.h>
-
-int printout(const char *edemede, ...);
-void display_prompt(void);
-int specification(const char *edemede, va_list(list_of_ndeputa));
-void execute_ozi(char *input);
-void ntisasi(char *webata, char *args[]);
-char *_strtok(char *str, const char *delim);
-char *_strchr(const char *str, int c);
-char *_getline(void);
-void uzor(const char *order, const char *path, char *outcome);
-extern char *args[100];
-extern char **environ;
+#include <fnctl.h>
 #endif
